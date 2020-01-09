259dd0ffa028896e90ab076fa3ca73941e4d6411
Jack Cole <123jack.cole@gmail.com>
2020-01-09 22:33:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   count = 0
   while count < array.length do
-    array[count] = array[count] ** 2
+    #array[count] = array[count] ** 2
+    array[count] = array[count] * array[count]
     count += 1
   end
 end
